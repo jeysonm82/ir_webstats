a96d1b4b645d484659711df9c8d3d127232934ab
Jason Dilworth <hello@jasondilworth.co.uk>
2018-09-05 21:45:59 +0100

    Setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
-setup(name='irwebstats',
+setup(name='ir_webstats',
       version='1.1',
-      package_dir={'': 'ir_webstats'},
+      package_dir={'ir_webstats': 'ir_webstats'},
       packages=[''],
       url='https://github.com/jasondilworth56/ir_webstats/',
       author='Jason Dilworth',
