37ede3477c53216c2f4800caf50aa0c578ff8e36
Che931 <fj.romero93@gmail.com>
2019-01-19 05:30:05 +0100

    Updated setup.py
    * Removed build folder
    * Setup.py uses setuptools and request lib has been added as dependency.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='ir_webstats',
-      version='1.1',
+      version='1.1.1',
       packages=['ir_webstats'],
       url='https://github.com/jasondilworth56/ir_webstats/',
       author='Jason Dilworth',
-      author_email='hello@jasondilworth.co.uk'
-     )
+      author_email='hello@jasondilworth.co.uk',
+      install_requires=['requests']
+      )
