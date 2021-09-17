4c80f5bd09cf4e3c2125298f350b1921470d9b45
Jason Dilworth <hello@jasondilworth.co.uk>
2021-09-17 09:35:33 +0100

    Push for reinstall

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ir_webstats',
-      version='1.1.1',
+      version='1.1.2',
       packages=['ir_webstats'],
       url='https://github.com/jasondilworth56/ir_webstats/',
       author='Jason Dilworth',
