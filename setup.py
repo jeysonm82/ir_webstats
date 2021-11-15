d2f7e4973a24a06feabec0c95ca0faa4158a68c5
Jason Dilworth <hello@jasondilworth.co.uk>
2021-11-15 23:02:29 +0000

    Bump setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ir_webstats',
-      version='1.1.2',
+      version='1.1.3',
       packages=['ir_webstats'],
       url='https://github.com/jasondilworth56/ir_webstats/',
       author='Jason Dilworth',
